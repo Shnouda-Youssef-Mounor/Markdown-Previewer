90b02a2550b02ca955224511fe80b5e5c976d546
unknown <shnoudayoussef7@gmail.com>
2023-08-30 16:11:26 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {marked} from 'marked'
+import useLocalStorage from './useLocalStorage';
 
-function App() {
+const App = () => {
+  
+  const [compiled, setCompiled] = useState('<h2 id="hello">Hello</h2>')
+  const [code, setCode] = useLocalStorage('## Hello',compiled);
+
+  const [hide, hidePreview] = useState(true)
+  
+  const openMD = () => {
+    console.log(0)
+    hidePreview(true)
+  }
+
+  const openPreview = () => {
+    console.log(0)
+    hidePreview(false)
+  }
+  const handleChange = (e) => {
+    setCode(e.target.value)
+    setCompiled(marked.parse(e.target.value))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>MarkDown Previewer React App</h1>
+      <div className="container">
+        <div className="btns">
+          <button onClick={openMD} className="tab-btn active" data-tab="markdow">MarkDown</button>
+          <button onClick={openPreview} className="tab-btn" data-tab="preview" >Preview</button>
+          <button onClick={openPreview} className="tab-btn" data-tab="docs" >Docs</button>
+        </div>
+        {
+        hide ? 
+          <div>
+            <textarea onChange={handleChange} value={code}/>
+          </div> : 
+          <div>
+            <textarea value={compiled}/>
+          </div>
+        }
+      </div>
+    </>
+  )
 }
+
 
 export default App;
